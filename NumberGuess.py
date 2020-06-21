8a2628ce296f6a5ed36db2f39a983f342344ee0b
Ravindu Induwara <ravinduinduwara@gmail.com>
2020-06-21 09:27:02 +0530

    added the game menu

--- NumberGuess.py
+++ NumberGuess.py
@@ -16,3 +16,27 @@ def register():
     return counter, playerName
 
 counter,players = register()
+scores = [0,0,0,0,0,0,0,0,0,0]
+
+
+def menu(scores):
+    print("\t\nWelcome to the Number Guessing Game\n\n")
+    print("1. Play the Game")
+    print("2. Scoreboard")
+    print("3. Print scores to a .txt file")
+    print("0. Exit Game")
+    choice = int(input("Enter Choice(0-3) :" ))
+    if choice == 0 :
+        exit()
+    elif choice == 1 :
+        #scores = game(scores)
+        #score(scores)
+        print("play game")
+    elif choice == 2 :
+        #score(scores)
+        print("scores")
+    elif choice == 3 :
+        print("file")
+        #export(scores)
+
+menu(scores)
